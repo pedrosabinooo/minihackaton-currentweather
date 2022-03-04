633b4f7dfbf2b3ef6183dee2e414e25302702d50
pedrosabinooo <pedrosabino@outlook.com>
2022-03-04 20:12:49 -0300

    Implementada as funções

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-let URL = "https://api.openweathermap.org/data/2.5/weather?lat=-23&lon=-45&appid=d914c82f2580398b7b13c199a6c77966&lang=pt_br"
-let climateData = {};
-// Pedir localização do usuário
+let URL = {
+    beg: "https://api.openweathermap.org/data/2.5/weather?lat=",
+    mid: "&lon=",
+    end: "&appid=d914c82f2580398b7b13c199a6c77966&lang="
+};
 
+let climateData = {};
+let latitude;
+let longitude;
+let urlAxios;
+// Pedir localização do usuário
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        urlAxios = URL.beg+latitude.toFixed(0)+URL.mid+longitude.toFixed(0)+URL.end+navigator.language.replace("-","_").toLowerCase();
+        getClimateData();
+      });
+} else {
+    let location = prompt("Desculpe-me! O seu navegador não suporta a geolocalização. Coloque a sua latitude e longitude separados por ';'.").split(';');
+    latitude = parseInt(location[0]).toFixed(0);
+    longitude = parseInt(location[1]).toFixed(0);
+    urlAxios = URL.beg+latitude+URL.mid+longitude+URL.end+navigator.language.replace("-","_").toLowerCase();
+    getClimateData();
+}
 
 // Retornar dados da API
 function getClimateData(){
-    let promise = axios.get(URL);
+    let promise = axios.get(`${urlAxios}`);
     promise.then((response) => { 
         climateData = response.data 
-        teste()
         renderClimate()});
-    promise.catch(() => { window.location.reload() });
+    promise.catch(() => {  });
 }
-getClimateData()
-function teste(){console.log(climateData)}
 
 // Criar HTML dinâmico
 function renderClimate(){
     let divContent = document.querySelector("main")
+    
     divContent.innerHTML += `
     <div>
         <img src="http://openweathermap.org/img/wn/${climateData.weather[0].icon}@2x.png" alt="">
@@ -29,8 +48,8 @@ function renderClimate(){
     </div>
     <div>
         <p><strong>${climateData.name}, ${climateData.sys.country}</strong></p>
-        <p>Temp max: ${climateData.main.temp_max}</p>
-        <p>${climateData.weather[0].description}</p>
+        <p>${getTime(climateData.dt)}</p>
+        <p>${climateData.weather[0].description.toProperCase()}</p>
     </div>
     `
 }
@@ -39,6 +58,19 @@ function toCelsius(temp){
     return (temp-273).toFixed(0) + "°C";
 }
 
-function toLocalTime(time){
-    return time+climateData.sys.timezone;
+function getTime(timestamp){
+    date = new Date(timestamp * 1000),
+    datevalues = [
+        date.getFullYear(),
+        date.getMonth()+1,
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+    ];
+    return `${datevalues[2]}/${datevalues[1]}/${datevalues[0]} ${datevalues[3]}:${datevalues[4]}`;
 }
+
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
